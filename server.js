2820fff1591d5165c739b8f9fd268c041dc98d76
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2018-09-15 16:58:42 +0530

    changes to add csv

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const moment = require('moment');
+const multer = require('multer');
+const csv = require('fast-csv');
+const fs = require('fs');
 
 const app = express();
+var data = 'Helow';
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
@@ -12,25 +16,50 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.locals.moment = moment;
 
-var active = {
-		isActiveD: false,
-		isActiveO: false,
-		isActiveR: false,
-		isActiveP: false,
-		isActiveA: false,
-		isActiveB: false
-	};
-var isActivePD = false,isActivePO = false,isActivePP = false,isActivePS = false,isActivePI = false, isActivePR = false, isActivePS = false, isActivePRO = false;
+var upload = multer({dest: 'uploads/'});
+
 app.get('/pharmacy_login', (req, res, next) => {
 	res.render('login');
 });
 
 app.post('/login', (req, res, next) => {
 	if(req.body.email == 'test' && req.body.pharmaId == '2008'){
-		res.redirect('/');
+		res.redirect('/pharmacy');
 	} else {
 		res.redirect('/pharmacy_login');
 	}
+});
+
+
+app.get('/pharmacy', (req, res, next) => {
+	date = Date.now();
+	const title = 'Dashboard';
+	res.render('index',
+	{
+		date: date, 
+		deliverOrders: [],
+		title: title
+	});
+});
+
+app.get('/pharmacy_orders', (req, res, next) => {
+	const title = 'Orders';
+	res.render('pharmacy_orders', 
+	{
+		title: title
+	});
+});
+
+app.get('/pharmacy_product', (req, res, next) => {
+	const title = 'Product';
+	res.render('pharmacy_products', 
+	{
+		title: title
+	});
+});
+
+app.get('/logout', (req, res, next) => {
+	res.redirect('/pharmacy_login');
 });
 
 app.get('/distributor_login', (req, res, next) => {
@@ -49,40 +78,51 @@ app.get('/distributor_logout', (req, res, next) => {
 	res.redirect('/distributor_login');
 });
 
-app.get('/pharmacy_orders', (req, res, next) => {
-	res.render('pharmacy_orders');
-});
-
-app.get('/pharmacy_product', (req, res, next) => {
-	res.render('pharmacy_products');
-});
-
-app.get('/logout', (req, res, next) => {
-	res.redirect('/pharmacy_login');
-});
-
 app.get('/distributor', (req, res, next) =>{
 	date = Date.now();
-	active.isActiveD = true;
 	res.render('distributor_dashboard', 
 	{	
 		date: date, 
-		active: active
+		title: 'Dashboard'
 	});
+});
+
+app.post('/upload', upload.single('csvdata'), function (req, res, next) {
+	const fileRows = [];
+  
+	// open uploaded file
+	csv.fromPath(req.file.path)
+	  .on("data", function (data) {
+		fileRows.push(data); // push each row
+	  })
+	  .on("end", function () {
+		data = fileRows;
+		res.redirect('/distributor_product');
+		fs.unlinkSync(req.file.path);   // remove temp file
+		//process "fileRows" and respond
+	  })
+});
+
+app.get('/distributor_product', upload.single('csvdata'),(req, res, next) => {
+	if(data == 'Helow'){
+		console.log('Not');
+	res.render('distributor_product', 
+	{
+		title: 'Products',
+		data: data
+	}); }else {
+	res.render('distributor_product', 
+	{
+		title: 'Products',
+		data: data[0],
+		data1: data.slice(0)
+		});	
+}
 });
 
 app.get('/distributor_review', (req, res, next) => {
+	const title = 'Review';
 	res.render('distributor_review');
-});
-
-app.get('/', (req, res, next) => {
-	date = Date.now();
-	isActivePD = true;
-	res.render('index',
-	{
-		date: date, 
-		deliverOrders: []
-	});
 });
 
 app.listen(port, function() {
