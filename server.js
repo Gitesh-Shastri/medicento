af081ab52d39c12781d199116bc51cbf04a914cb
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2018-09-18 11:04:24 +0530

    changes

--- server.js
+++ server.js
@@ -118,7 +118,8 @@ app.get('/distributor_product', upload.single('csvdata'),(req, res, next) => {
 	res.render('distributor_product', 
 	{
 		title: 'Inventoy Product',
-		data: datah
+		data: datah,
+		product1: []
 	}); }else {
 		var data = datah;
 		datah = 'Helow';
@@ -128,7 +129,7 @@ app.get('/distributor_product', upload.single('csvdata'),(req, res, next) => {
 		title: 'Inventoy Product',
 		data: data[1],
 		data1: data.slice(2, 20),
-		product1: pro.slice(0, 8000) 
+		product1: pro.slice(0, 10000) 
 		});	
 	} 
 });
