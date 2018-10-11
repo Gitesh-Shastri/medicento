3f514b8be8a5da2bd7f94176063ad32ff744d21a
Gitesh-Shastri <40106556+Gitesh-Shastri@users.noreply.github.com>
2018-10-11 07:44:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const SalesOrder = require('./models/SalesOrder');
 const SalesOrderItems = require('./models/SalesOrderItem');
 const Products = require('./models/productandmedi');
 const Product = require('./models/Product');
+const message = require('./models/message');
 const Inventoy = require('./models/InventoryProduct');
 
 mongoose.connect(MONGODB_URI, function () {
@@ -157,20 +158,42 @@ app.post('/upload', upload.single('csvdata'), function (req, res, next) {
 	// open uploaded file
 	csv.fromPath(req.file.path)
 	  .on("data", function (data) {
-		if(data[0] != '' && data[2] != '' && data[10] == ''){
-			comp.push(data[2]);
-		}
-		if(data[0] != '' && data[2] != '' && data[10] != '') {
-			data[3] = comp[comp.length-1];
-			product.push(data);
-		} 
 		fileRows.push(data); // push each row
-		})
+		if(data[0] != 'Item name') {
+		const inventoryProduct = new InventoryProduct();
+        inventoryProduct.inventory_product_id =  new mongoose.Types.ObjectId();
+        inventoryProduct.inventory_id =  "5b2e2e31f739e00600387bdf";
+        inventoryProduct.product_name = data[0];
+        inventoryProduct.stock_left = data[3];
+		inventoryProduct.offer = "-";
+		inventoryProduct.save();
+    const product = new Product();
+        product.product_id = new mongoose.Types.ObjectId();
+        product.medicento_name = data[0];
+        product.company_name = data[8];
+        product.total_stock = data[3];
+    product.save();
+    const productandmedi = new ProductAndMedi({
+        product_id: product._id,
+        inventory_product_id: inventoryProduct._id
+    });
+    productandmedi.save();
+    }
+	})
 	  .on("end", function () {
 		datah = fileRows;
 		pro = product;
+		message.findOne({ _id: '5b98d26935df6d070c33dd46'}).exec().then( dic => {
+			message.findOneAndUpdate({ _id: dic.id}, {$set:{count:dic.count+1}}, {new: true}).exec().then((doc, err) => {
+        		console.log(doc);
 			res.redirect('/distributor_product');
-			fs.unlinkSync(req.file.path);   // remove temp file	
+			fs.unlinkSync(req.file.path);
+			});
+		}).catch(err => {
+		console.log(err);
+			res.redirect('/distributor_product');
+			fs.unlinkSync(req.file.path);
+		});   // remove temp file	
 		//process "fileRows" and respond
 	  })
 });
